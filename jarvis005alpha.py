d32ce7a17495e08c553baabc6780a58b7df2f1d4
rausejop <rafael.ausejo@gmail.com>
2019-08-11 21:37:54 +0200

    Add files via upload

--- jarvis005alpha.py
+++ jarvis005alpha.py
@@ -5,8 +5,9 @@
 
 El estado actual de este prototipo contiene los primeros módulos de
 obtención automatizada de fuentes, en concreto:
-    GEO.EU.ES\exteriores.gob.es
-    GEO.EU.ES\defensa.gob.es\ccd')
+    geo.eu.es.dsn.gob.es
+    geo.eu.es.defensa.gob.es.ccdc
+    geo.eu.es.exteriores.gob.es
 	
 NOTA: esta versióin jarvis005a es una versión Alfa incompleta
       La última versión estable se encuentra en el siguiente enlace:
@@ -25,11 +26,11 @@ NOTA: esta versióin jarvis005a es una versión Alfa incompleta
 __all__ = ['r', 'a', 'p']
 __author__ = "Rafael Ausejo Prieto"
 __authors__ = "Rafael Ausejo Prieto and Rafael Ausejo Prieto"
-__copyright__ = "Copyright 2019, Rafael Ausejo Prieto"
+__copyright__ = "(c) 2019 Rafael Ausejo Prieto"
 __credits__ = ["Rafael Ausejo Prieto", "Rafael Ausejo Prieto"]
-__date__ = "6 de agosto de 2019"
+__date__ = "10/08/2019"
 __license__ = "GPL"
-__version__ = "005a"
+__version__ = "005a1"
 __maintainer__ = "Rafael Ausejo Prieto"
 __email__ = "rafael.ausejo@gmail.com"
 __status__ = "Prototype"
@@ -39,106 +40,36 @@ __status__ = "Prototype"
 import os    # Standard library
 import sys
 import configparser
+from datetime import datetime
 
 # Related third party imports
 # Local application/library specific imports
 
-# CONSTANTES
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36"
-WGET_PARAMS = "-c --no-check-certificate --content-disposition"
 
-
-# Fuente:    GEO.EU.ES\exteriores.gob.es
-source = 'Ministerio de Asuntos Exteriores. Gobierno de España'
-classification = 'A1'
-dir_base = 'D:\DATALAKE'
-dir_path = 'GEO.EU.ES\exteriores.gob.es'
-PAISES = [
-    "Alemania",
-    "Austria",
-    "Bélgica",
-    "Bulgaria",
-    "Chequia",
-    "Chipre",
-    "Croacia",
-    "Dinamarca",
-    "Eslovaquia",
-    "Eslovenia",
-    "España",
-    "Estonia",
-    "Finlandia",
-    "Francia",
-    "Grecia",
-    "Hungría",
-    "Irlanda",
-    "Italia",
-    "Letonia",
-    "Lituania",
-    "Luxemburgo",
-    "Malta",
-    "Países Bajos",
-    "Polonia",
-    "Portugal",
-    "Reino Unido",
-    "Rumanía",
-    "Suecia",
-    ]	
-
-URL = [
-    'http://www.exteriores.gob.es/Documents/FichasPais/ALEMANIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/AUSTRIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/BELGICA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/BULGARIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/REPUBLICACHECA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/CHIPRE_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/CROACIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/DINAMARCA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/ESLOVAQUIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/ESLOVENIA_FICHA%20PAIS.pdf',
-    'https://www.icex.es/icex/GetDocumento?dDocName=DAX2017729356&site=icexES -o DAX2017729356.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/ESTONIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/FINLANDIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/FRANCIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/GRECIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/HUNGRIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/IRLANDA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/ITALIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/LETONIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/LITUANIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/LUXEMBURGO_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/MALTA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/PAISESBAJOS_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/POLONIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/PORTUGAL_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/REINOUNIDO_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/RUMANIA_FICHA%20PAIS.pdf',
-    'http://www.exteriores.gob.es/Documents/FichasPais/SUECIA_FICHA%20PAIS.pdf',
-    ]
-
-# Función jarvis_direccion()
-def jarvis_direccion():
-    r""" Direction step, to be defined """ 
-    # Función de Planificación de Inteligencia, por definir
+# Función jarvis_direccion(configuration)
+def jarvis_direccion(file,dir):
+    r""" Creates Planification Structure """ 
+    # Lee la planificación y crea los directorios
+    now_time = datetime.now()
+    print (f"[*] {now_time} Starting structure creation")
+    print (f"[+] {dir}")
+#    os.chdir(f"{dir}")
+    with open(file, encoding='utf-8') as f:
+        for line in f:
+#        read_data = f.read()
+#        print(f"{read_data}")
+            line = line [0:len(line)-1]
+            line = f"{line}"
+            print(line)		
+    f.closed
+    now_time = datetime.now()	
+    print (f"[*] {now_time} Ending structure creation")    
+    # os.chdir({dir})
 
 
 # Función jarvis_obtencion()
 def jarvis_obtencion(configuration):
-    r""" Creates directory and discriminates file format """ 
-    # Carga el fichero de configuración con el índice de las fuentes
-    config = configparser.ConfigParser()
-    config.read(f'jarvis.ini')
-    source = config[configuration]['source']
-    classification = config[configuration]['classification']
-    dir_base = config[configuration]['dir_base']
-    dir_path = config[configuration]['dir_path']
-    file = config[configuration]['file']
-    fileformat = config[configuration]['fileformat']
-    print(f"FUENTE:            {source}")
-    print(f"CLASIFICACIÓN:     {classification}")
-    print(f"BASE:              {dir_base}")
-    print(f"RUTA:              {dir_path}")
-    print(f"FICHERO:           {file}")
-    print(f"FORMATO:           {fileformat}")	
+    r""" Creates directoriesy and discriminates file format """ 
 
     # Crea el directorio y discrimina el formato del fichero de fuente
     old_path = os.getcwd()
@@ -154,24 +85,6 @@ def jarvis_obtencion(configuration):
     if not os.path.exists(directory):
         os.makedirs(directory)
     os.chdir(directory)
-
-    # Discriminación de formatos
-    print ({fileformat})
-    if ({fileformat}) == "lista":
-        print("Formato: lista")
-
-    elif ({fileformat}) == "directorios":
-        print("Formato: directorios")
-        for i in range(len(PAISES)):
-            descarga_directorios(PAISES[i], URL[i])
-            # Vuelve al direcorio base
-            os.chdir(directory)
-
-    else:
-        print("Formato: <desconocido>")
-
-    # Vuelve a direcorio raíz
-    os.chdir(old_path)
 
 
 # Función descarga_directorios() para jarvis_obtencion()
@@ -191,16 +104,53 @@ def descarga_directorios(directorio, url):
     print (f"{command}")
 
 
+# Función jarvis_direccion()
+def jarvis_elaboracion():
+    r""" Elaboration step, to be defined """ 
+    # Función de Elaboración de Inteligencia, por definir
+    pass
+
+
+# Función jarvis_difusion()
+def jarvis_difusion():
+    r""" Dissemination step, to be defined """ 
+    # Función de Difusión de Inteligencia, por definir
+    pass
+
+
 # Función main()
 def main(): 
     r""" Main program, implementing Intelligence Cycle steps """ 
-    # Programa principal con las fases del Ciclo de Inteligencia
-    jarvis_direccion()
-    jarvis_obtencion(configuration = 'DEFAULT')
-    jarvis_obtencion(configuration = 'GEO.EU.ES\exteriores.gob.es')
-    jarvis_obtencion(configuration = 'GEO.EU.ES\defensa.gob.es\ccd')
+    # Programa principal con las variables globales
+    old_path = os.getcwd()
+    print(f"")
+    print(f"JARVIS v{__version__} {__copyright__} [{__date__}]")
+    start_time = datetime.now()
+    now_time = start_time
+    print (f"[*] {now_time} Configuration starts")
+    config = configparser.ConfigParser()
+    config.read(f'jarvis.ini')
+    configuration = 'DEFAULT'
+    dir_base = config[configuration]['dir_base']
+    print (f"[+] {dir_base}")
+    jarvis_structure = config[configuration]['jarvis_structure']
+    print (f"[+] {jarvis_structure}")	
+    wget_params = config[configuration]['wget_params']
+    print (f"[+] wget {wget_params}")
+    wget_user_agent = config[configuration]['wget_user_agent']
+    print (f"[+] {wget_user_agent}")
+    end_time = datetime.now()
+    print (f"[*] {end_time} Configuration ends")
+    jarvis_direccion(jarvis_structure, dir_base)
+#    jarvis_obtencion(configuration = 'DEFAULT')
 #    jarvis_elaboracion()
 #    jarvis_difusion()
+
+    os.chdir(old_path)
+    end_time = datetime.now()
+    print (f"[*] {end_time} Program ends")
+    total_time = end_time - start_time
+    print (f"[*] Finished in {total_time}")
 
 
 # Comienzo del programa main()
